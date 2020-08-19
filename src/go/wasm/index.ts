96d1f681b86d271c80ebc774bcd1dd98f4128949
hiennguyen-me <nguyenthithuhien249@gmail.com>
2020-08-19 11:37:03 +0700

    Update load wasm on browser

--- src/go/wasm/index.ts
+++ src/go/wasm/index.ts
@@ -30,16 +30,16 @@ async function _loadWasmOnBrowser(resolve: Function, reject: Function) {
   try {
     const go = new Go();
 
-    if (!WebAssembly.instantiateStreaming) { // polyfill
-      WebAssembly.instantiateStreaming = async (resp, importObject) => {
-        try {
-          const source = await (await resp).arrayBuffer();
-          return await WebAssembly.instantiate(source, importObject);
-        } catch (e) {
-          reject(e);
-        }
-      };
-    }
+    // if (!WebAssembly.instantiateStreaming) { // polyfill
+    //   WebAssembly.instantiateStreaming = async (resp, importObject) => {
+    //     try {
+    //       const source = await (await resp).arrayBuffer();
+    //       return await WebAssembly.instantiate(source, importObject);
+    //     } catch (e) {
+    //       reject(e);
+    //     }
+    //   };
+    // }
     const result = await WebAssembly.instantiateStreaming(fetch(getConfig().wasmPath || fileName).catch(e => reject(e)), go.importObject);
     const inst = result.instance;
     go.run(inst);
